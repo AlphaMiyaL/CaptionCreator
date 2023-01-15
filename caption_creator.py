4e3203c619e9042fcb597df9f3e1ae4e2136e84f
Brian Song <79394580+AlphaMiyaL@users.noreply.github.com>
2023-01-15 05:59:25 -0800

    rekog is now a class; moderation labels added

--- caption_creator.py
+++ caption_creator.py
@@ -1,32 +1,10 @@
 import csv
 import boto3
+from rekognition import Rekognition
 
-# csv file is the credentials for the IAM account
-# the IAM account should have FULL access to Amazon Rekognition and S3 Bucket to work properly
-with open('new_user_credentials.csv', 'r') as input:
-    next(input)
-    reader = csv.reader(input)
-    for line in reader:
-        access_key_id = line[2]
-        secret_access_key = line[3]
-
-# Images should only be .jpg or .png format
-# Alice.jpg is a test image
-photo = 'Alice.png'
-
-client = boto3.client('rekognition',
-                      region_name='us-west-2',
-                      aws_access_key_id=access_key_id,
-                      aws_secret_access_key=secret_access_key)
-with open(photo, 'rb') as source_image:
-    source_bytes = source_image.read()  # Convert image to bytes
-
-response = client.detect_labels(Image={'Bytes': source_bytes},  # if taking img from project src, use this line
-                                # Image={'S3Object': {  # if taking img from S3 Bucket, use these lines
-                                #     'Bucket': '',  # name of the S3 bucket
-                                #     'Name': '',  # name of the img file in the bucket
-                                #     'Version': ''  # only needed of bucket has versioning enabled
-                                # }},
-                                MaxLabels=10,  # limits the labels returned
-                                MinConfidence=95)  # returns only labels that have confidence level > amount specified
-print(response)
+rk_client = Rekognition('new_user_credentials.csv')
+rk_client.replace_photo('Alice.png')
+rk_client.detect_labels(10, 95)
+rk_client.print_labels()
+rk_client.detect_moderation_labels(95)
+rk_client.print_labels()
---
+++ rekognition.py
@@ -0,0 +1,55 @@
+import csv
+import boto3
+
+
+class Rekognition:
+    def __init__(self, csv_file):
+        # csv file is the credentials for the IAM account
+        # the IAM account should have FULL access to Amazon Rekognition and S3 Bucket to work properly
+        with open(csv_file, 'r') as input:
+            next(input)
+            reader = csv.reader(input)
+            for line in reader:
+                access_key_id = line[2]
+                secret_access_key = line[3]
+        self.client = boto3.client('rekognition',
+                                   region_name='us-west-2',
+                                   aws_access_key_id=access_key_id,
+                                   aws_secret_access_key=secret_access_key)
+        self.photo = None
+        self.source_bytes = None
+        self.response = None
+
+    # Images should only be .jpg or .png format
+    # Alice.jpg is a test image
+    def replace_photo(self, photo):
+        self.photo = photo
+        with open(photo, 'rb') as source_image:
+            self.source_bytes = source_image.read()  # Convert image to bytes
+
+    def detect_labels(self, max_labels, min_confidence):
+        self.response = self.client.detect_labels(
+            Image={'Bytes': self.source_bytes},  # if img from proj src, use this line
+            # Image={'S3Object': {  # if img from S3 Bucket, use these lines
+            #     'Bucket': '',  # name of the S3 bucket
+            #     'Name': '',  # name of the img file in the bucket
+            #     'Version': ''  # only needed of bucket has versioning enabled
+            # }},
+            MaxLabels=max_labels,  # limits the labels returned
+            MinConfidence=min_confidence)  # returns only labels with confidence >
+
+    # use detect_moderation_labels if wanting to moderate photos of explicit content
+    # moderation labels return labels that are classified as suggestive or explicit, moderator can decide what to do
+    # contains minConfidence as a parameter to return, no maxLabels
+    def detect_moderation_labels(self, min_confidence):
+        self.response = self.client.detect_moderation_labels(
+            Image={'Bytes': self.source_bytes},  # if img from proj src, use this line
+            # Image={'S3Object': {  # if img from S3 Bucket, use these lines
+            #     'Bucket': '',  # name of the S3 bucket
+            #     'Name': '',  # name of the img file in the bucket
+            #     'Version': ''  # only needed of bucket has versioning enabled
+            # }},
+            MinConfidence=min_confidence)  # returns only labels with confidence >
+
+    def print_labels(self):
+        print(self.response)
