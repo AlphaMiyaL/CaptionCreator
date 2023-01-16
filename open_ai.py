ae5c28b7aa88445488bf7f8caf8477e51746f582
Selim <selim_frljuckic@yahoo.com>
2023-01-16 11:37:42 -0800

    Added OpenAI Caption Class + Modified Existing Caption Creator Class

---
+++ open_ai.py
@@ -0,0 +1,19 @@
+import openai
+
+
+class OpenAi:
+    def __init__(self):
+        # Initialize the openai API key
+        openai.api_key = "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"
+
+    def caption(self, labels):
+        # Build the prompt for GPT-3
+        prompt = f"Caption for an image with labels: {', '.join(label['Name'] for label in labels['Labels'])}"
+
+        # Generate a caption using GPT-3
+        response = openai.Completion.create(engine="text-davinci-002", prompt=prompt)
+
+        # Extract the generated caption
+        caption = response["choices"][0]["text"]
+
+        print(caption)
